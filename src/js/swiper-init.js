858a6afb44a496710bfa355dc112e9cfc25b8693
Oleksandr <live.gp@gmail.com>
2023-03-18 00:27:48 +0200

    first push

--- src/js/swiper-init.js
+++ src/js/swiper-init.js
@@ -2,9 +2,9 @@
         pagination: {
           el: ".swiper-pagination",
           clickable: true,
-          renderBullet: function (index, className) {
-            return '<span class="' + className + '">'  + "</span>";
-          },
+          // renderBullet: function (index, className) {
+          //   return '<span class="' + className + '">'  + "</span>";
+          // },
     },
     slidesPerView:'auto',
       });
