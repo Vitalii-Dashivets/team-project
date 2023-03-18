7e03be52adb4e1fd83c2ca5396c037681f744537
MishaWesT <westbeast92@hotmail.com>
2023-03-18 10:29:30 +0200

    fix

--- src/js/swiper-init.js
+++ src/js/swiper-init.js
@@ -1,10 +1,10 @@
-  var swiper = new Swiper(".swiper", {
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-          renderBullet: function (index, className) {
-            return '<span class="' + className + '">'  + "</span>";
-          },
-        },
-        
-      });
+var swiper = new Swiper('.swiper', {
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + '</span>';
+    },
+  },
+  slidesPerView: 'auto',
+});
